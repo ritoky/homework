6c2f85c1c6013591ea41b2c9468fa1a3352b3c9a
Denis <122354607+ritoky@users.noreply.github.com>
2024-05-18 15:13:54 +0300

    Add files via upload

---
+++ Подсказки.py
@@ -0,0 +1,82 @@
+# print()   (вывод  на экран)
+print('Hello, world!')
+
+# type() (тип числа)
+print(type(5))
+
+# str - sring (строка)
+print(type("a"))
+
+# int - integer (целое число)
+print(type(2))
+
+# float (числа с плавающей запятой)
+print(type(2.0))
+
+# + (плюс)
+print(2 + 2)
+
+# - (минус)
+print(2 - 2)
+
+# * (умножение)
+print(2 * 2)
+
+# ** (возведение в степень)
+print(5 ** 2)
+
+# / (деление)
+print(2 / 2)
+
+# // (Целичное деление)
+print(4 // 2)
+
+# % (остаток)
+print(5 % 2)
+
+# concatenate (сложение строк)
+print('Hello, ' + 'world!')
+
+# True, False - boolean (логический тип данных)
+print(True, False)
+print(type(True), type(False))
+print(5 < 10)  # false - правда
+print(5 > 10)  # True - ложь
+
+# == (равны)
+print(5 == 5)
+
+# >= (больше или равны)
+print(5 >= 5)
+
+# <= (меньше или равны)
+print(5 <= 5)
+
+# != (неравны)
+print(5 != 5)
+
+# or (или)
+print(5 == 5 or 5 < 3)
+print(5 == 5 or 5 == 3)
+
+# and (и)
+print(5 == 5 and 5 < 3)
+print(5 == 5 and 5 > 3)
+
+# перевод
+print(float('5'))
+print(int('5'))
+print(type(bool('5')))
+
+you_name = 'Denis' #Snake case (раздиление слов подчеркиванием)
+sapSap = 'Hello, ' #Camel case (раздиление слов заглавными буквами)
+print(sapSap + you_name)
+        # 123456789101112
+name = 'это моя строка'
+        # 012345678910111213
+print(name [0])
+print(name [-1])
+print(name [1:5])
+print(name[::-1])
+print(len (name))
+print('это новая строка' + 'это моя строка')
