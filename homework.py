3ace6c9d5133d525c5b914bedb076c4de9a228e2
Denis <122354607+ritoky@users.noreply.github.com>
2024-11-17 15:53:43 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,16 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a)
+print(b)
+print(a == b)
+
+# 4st program
+a = float('123.456')
+print(int(a * 10) * 10 // 10)
